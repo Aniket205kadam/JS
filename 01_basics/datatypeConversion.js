e9755db39ecb99468463616aec10c4933e4fa66e
Aniket205kadam <aniketrkadam205@gmail.com>
2024-08-02 20:40:06 +0530

    conversion

--- 01_basics/datatypeConversion.js
+++ 01_basics/datatypeConversion.js
@@ -34,8 +34,73 @@ let boolValue = true
 let stringNumber = String(someNumber)
 let stringBool = String(boolValue)
 
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
 
-console.log(stringBool)
-console.log(typeof(stringBool))
+// console.log(stringBool)
+// console.log(typeof(stringBool))
+
+// **************************** Operation ************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2**2) //2^2
+
+let str1 = "hello"
+let str2 = " aniket"
+let str3 = str1 + str2
+// console.log(str3)
+
+/*
+//var vs let
+var varName = "java"
+let letName = "java"
+
+
+if (true) {
+    var varName = "javascript"
+    let letName = "javascript"
+    console.log("Inside scope varName: " + varName)
+    console.log("Inside scope letName: " + letName)
+}
+
+console.log("outside scope varName: " + varName)
+console.log("outside scope letName: " + letName)
+*/
+
+//string converstions
+
+// console.log(1 + "2") //12
+// console.log("1" + 2) //12
+// console.log("1" + 2 + 2) //122
+// console.log(2 + 2 + "1") //41
+// console.log((3 + 4) * (5 % 3)) //14
+
+// console.log(true)//true
+// console.log(+true)//1
+// console.log(true + false) //1 => 1 + 0
+// console.log(true + 1) //2
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+// console.table([num1, num2, num3])
+
+//pre or post
+//inc or dec
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+console.log("post")
+gameCounter = 100
+console.log(gameCounter++)
+console.log(gameCounter)
+
+console.log("pre")
+gameCounter = 100
+console.log(++gameCounter)
+console.log(gameCounter)
