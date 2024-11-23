871d4c1d4d402dae8d5cb525638435553c1203d1
Aniket205kadam <aniketrkadam205@gmail.com>
2024-11-23 16:12:34 +0530

    revision

---
+++ 01_basics/revision.js
@@ -0,0 +1,57 @@
+"use strict";
+
+{
+    let num = 2;
+    var num1 = "three";
+    const num2 = "four";
+
+    // console.table([num, num1, num2]);
+}
+
+// console.log("\\")
+
+// console.log(num);
+// console.log(num1);
+// console.log(num2);
+
+// let obj = {rollNo: 1, name: "aniket"};
+
+let obj = ['aniket', 'rohit', 'virat'];
+
+// console.log(typeof obj)
+
+// console.log(typeof undefined)
+
+const bigNumber = 1234567890123456789012345678901234567890n;
+
+// console.log(typeof bigNumber)
+
+// function ad() {
+//     console.log("function");
+// }
+
+// console.log(typeof ad);
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.table([id, anotherId]);
+
+// console.log(id == anotherId);
+
+const name1 = "aniket";
+const name2 = "aniket";
+
+// console.log(name1 === name2);
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+console.log("heros: " + typeof(heros))
+
+let fubction1 = function(){
+    console.log("function()")
+};
+
+fubction1();
+
